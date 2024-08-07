9fc5cd51c51d344986eb20fb442f9f07850c2655
Blake Lockley <bwlockley@gmail.com>
2024-08-07 16:22:06 +1000

    Make FAQ left aligned

--- src/FAQModal.tsx
+++ src/FAQModal.tsx
@@ -17,10 +17,10 @@ function FAQModal(props: { faq?: string, showFAQ: boolean, setShowFAQ: (f: boole
     <Dialog open={props.showFAQ} onClose={onClose} className="relative z-50">
       <div className="fixed inset-0 flex w-screen h-screen items-center justify-center bg-black/50">
         <Dialog.Panel className="w-full md:w-[600px] relative aspect-square flex flex-col items-center text-center p-20" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/scroll.png)`, backgroundSize: "contain", backgroundRepeat: "no-repeat" }}>
-          <div className="text-white text-2xl z-10">
+          <div className="text-white text-2xl z-10 mb-2">
             FAQ
           </div>
-          <div className='flex flex-col my-auto w-[400px] whitespace-pre-line overflow-y-auto text-white h-[400px] pr-4 -mr-4'>
+          <div className='flex flex-col my-auto w-[400px] h-[400px] whitespace-pre-line overflow-y-auto text-white pr-4 -mr-4 text-left'>
             {props.faq ?? "Loading..."}
           </div>
           <div className='absolute right-4 top-4 flex text-red-400 rounded-full z-10'>
